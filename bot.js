c86452e809194e1252ea6af4fcdb4773787d86b7
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-12 20:46:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,8 +10,6 @@ const YouTube = require('simple-youtube-api');
 
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 
-const queue = new Map();
-
 const ytdl = require('ytdl-core');
 
 const fs = require('fs');
