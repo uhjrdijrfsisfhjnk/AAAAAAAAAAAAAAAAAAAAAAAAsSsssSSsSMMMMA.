a8ddcdf15b4649c42cbadcb1cc75e96484bfd138
RaiVOexe <40840059+RaiVOexe@users.noreply.github.com>
2018-08-13 18:46:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = 'Aa';
 client.on('ready', function() {
+client.setStatus('idle')
     console.log(`i am ready ${client.user.username}`);
 });
 
